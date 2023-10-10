bc162cfc3a54b0a9a61035b7dd627dd27660b3e2
Cha <playerred33@gmail.com>
2023-10-10 18:20:39 +0900

    styed commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Body = styled.body`
+  height: 100vh; display: flex; align-items: center; justify-content: center; flex-direction: column;
+`
+const ContainerHeader = styled.header `
+  
+`
+const span = styled.span`
+  display:none;
+`
+
+const SubmitButton = styled.button`
+  width:150px; height:50px; text-align:center;
+  color:#fff; cursor:pointer; border:0px;
+  letter-spacing:1px; font-size:18px; border-radius: 5px;
+  background: linear-gradient(0deg, rgba(0,172,238,1) 0%, rgba(2,126,251,1) 100%);
+  box-shadow: inset 2px 2px 2px 0px rgba(255,255,255,.5), 7px 7px 20px 0px rgba(0,0,0,.1), 4px 4px 5px 0px rgba(0,0,0,.1);
+`
+const SignupButton = styled.button`
+  width:150px; height:50px; text-align:center;
+  color:#fff; cursor:pointer; border:0px;
+  letter-spacing:1px; font-size:18px; border-radius: 5px;
+  background: linear-gradient(0deg, rgba(96,9,240,1) 0%, rgba(129,5,240,1) 100%);
+  box-shadow: inset 2px 2px 2px 0px rgba(255,255,255,.5), 7px 7px 20px 0px rgba(0,0,0,.1), 4px 4px 5px 0px rgba(0,0,0,.1);
+`
+function body(){
+  return
+}
 
 function Header(){
-  return <header>
+  return <ContainerHeader>
     <h1><a href="#"><img src="img/logo.png" />FirstClub</a></h1>
-  </header>
+  </ContainerHeader>
 }
 function Section(){
   return <section>
@@ -20,17 +49,17 @@ function Section(){
       </div>
     </div>
     <div className="btnbox">
-        <button type="submit">로그인test</button>
-        <button type="sign-up">회원가입</button>
+        <SubmitButton>로그인</SubmitButton>
+        <SignupButton>회원가입</SignupButton>
     </div>
   </section>
 }
 function App() {
   return (
-    <div>
+    <Body>
       <Header></Header>
       <Section></Section>
-    </div>
+    </Body>
   );
 }
 
